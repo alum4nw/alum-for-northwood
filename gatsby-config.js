d2a746ead6b059b3a8a3d2f730e5db7ad76b991a
virenabhyankar <forvirenra@gmail.com>
2021-09-21 17:19:14 -0700

    Remove source filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,13 +8,6 @@ module.exports = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages/`,
-      },
-    },
     "gatsby-plugin-styled-components",
     "gatsby-plugin-postcss",
     {
