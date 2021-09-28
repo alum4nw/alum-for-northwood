6372985ac90d16853eb78253960ed910e27b5cf5
virenabhyankar <forvirenra@gmail.com>
2021-09-27 19:37:21 -0700

    Add transform hover

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -3,7 +3,10 @@ import * as React from "react";
 const AboutCard = ({ image, titleText, bodyText }) => {
   return (
     <div className="container w-80 group">
-      <div className="bg-white flex flex-col text-left border border-black rounded-xl p-6 hover:shadow-custom">
+      <div
+        className="bg-white flex flex-col text-left border border-black rounded-xl p-6 
+      hover:shadow-custom transform hover:-translate-x-1 hover:-translate-y-1"
+      >
         {image}
         <h4 className="text-blue-dark font-body text-h4 group-hover:text-blue pt-5 pb-2">
           {titleText}
