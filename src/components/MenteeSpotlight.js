e4c2db2228eb1128a10783ef97ab743cba18580c
virenabhyankar <forvirenra@gmail.com>
2021-10-05 01:35:45 -0700

    Resize mentee spotlight

--- src/components/MenteeSpotlight.js
+++ src/components/MenteeSpotlight.js
@@ -8,13 +8,13 @@ const MenteeSpotlight = () => {
     <div className="bg-blue-lightest flex flex-col py-16 justify-center items-center">
       <h2 className="text-h2 font-header">Meet Mentee Mia Lee!</h2>
       <div className="py-11 flex flex-row justify-center items-center ">
-        <PencilPaperclip className="w-1/4" />
-        <div className="px-11 w-5/12">
+        <PencilPaperclip className="w-1/12 h-2/3" />
+        <div className="px-11 w-1/4">
           <StaticImage src="../images/menteeSpotlight.png" />
         </div>
-        <Gavel className="w-1/4" />
+        <Gavel className="w-1/12 h-2/3" />
       </div>
-      <p className="font-body text-body w-1/2">
+      <p className="font-body text-body w-1/3">
         "I initially joined ALUM because it seemed like an interesting
         organization, and I wished to garner as much support as possible while
         entering high school. I never expected for it to be such a wholesome
