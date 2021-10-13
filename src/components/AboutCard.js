74a9a1ce3e6289ca297dac6dda5b6dc4b7ba2206
virenabhyankar <forvirenra@gmail.com>
2021-10-13 12:17:29 -0700

    Refactor about card and learn more

--- src/components/AboutCard.js
+++ src/components/AboutCard.js
@@ -1,20 +1,42 @@
 import * as React from "react";
+import { Link } from "gatsby";
 
-const AboutCard = ({ image, titleText, bodyText }) => {
-  return (
-    <div className="container w-80 group">
-      <div
-        className="bg-white flex flex-col text-left border border-black rounded-xl p-6 
+const AboutCard = ({ image, titleText, bodyText, linkTo, isInternal }) => {
+  if (isInternal) {
+    return (
+      <div className="container w-3/4 md:w-80 group">
+        <Link to={linkTo}>
+          <div
+            className="bg-white flex flex-col text-left border border-black rounded-xl p-6 
       hover:shadow-custom transform hover:-translate-x-1 hover:-translate-y-1"
-      >
-        {image}
-        <h4 className="text-blue-dark font-body text-h4 group-hover:text-blue pt-5 pb-2">
-          {titleText}
-        </h4>
-        <p className="font-body text-body h-16">{bodyText}</p>
+          >
+            {image}
+            <h4 className="text-blue-dark font-body text-mh4 md:text-h4 group-hover:text-blue pt-5 pb-2">
+              {titleText}
+            </h4>
+            <p className="font-body text-body h-16">{bodyText}</p>
+          </div>
+        </Link>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="container w-3/4 md:w-80 group">
+        <a href={linkTo} target="_blank" rel="noreferrer noopener">
+          <div
+            className="bg-white flex flex-col text-left border border-black rounded-xl p-6 
+      hover:shadow-custom transform hover:-translate-x-1 hover:-translate-y-1"
+          >
+            {image}
+            <h4 className="text-blue-dark font-body text-mh4 md:text-h4 group-hover:text-blue pt-5 pb-2">
+              {titleText}
+            </h4>
+            <p className="font-body text-body h-16">{bodyText}</p>
+          </div>
+        </a>
+      </div>
+    );
+  }
 };
 
 export default AboutCard;
--- src/components/LearnMore.js
+++ src/components/LearnMore.js
@@ -1,7 +1,6 @@
 import * as React from "react";
 import AboutCard from "./AboutCard";
 import { StaticImage } from "gatsby-plugin-image";
-import { Link } from "gatsby";
 
 const LearnMore = () => {
   return (
@@ -9,50 +8,46 @@ const LearnMore = () => {
       <h2 className="font-header text-mh2 md:text-h2 pt-10 md:pt-16 md:pb-5">
         Learn more about...
       </h2>
-      <div className="flex flex-col md:flex-row space-y-8 md:space-y-0 md:pb-10 md:space-x-9">
-        <Link to="/program">
-          <AboutCard
-            image={
-              <StaticImage
-                src="../images/ourProgram.png"
-                alt="our-program"
-                className="border border-black"
-              />
-            }
-            titleText="Our Program"
-            bodyText="Get 1-on-1 mentorship and career guidance through our summer program."
-          />
-        </Link>
-        <Link to="/about">
-          <AboutCard
-            image={
-              <StaticImage
-                src="../images/ourTeam.png"
-                alt="our-program"
-                className="border border-black"
-              />
-            }
-            titleText="Our Leadership Team"
-            bodyText="Find out how we got started and what we're all about."
-          />
-        </Link>
-        <a
-          href="https://mentorcenter.us/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          <AboutCard
-            image={
-              <StaticImage
-                src="../images/ourMentors.png"
-                alt="our-program"
-                className="border border-black"
-              />
-            }
-            titleText="Our Mentors"
-            bodyText="Meet an amazing community of alumni and schedule mentorship sessions."
-          />
-        </a>
+      <div className="flex flex-col items-center justify-center md:flex-row space-y-8 md:space-y-0 md:pb-10 md:space-x-9">
+        <AboutCard
+          image={
+            <StaticImage
+              src="../images/ourProgram.png"
+              alt="our-program"
+              className="border border-black"
+            />
+          }
+          titleText="Our Program"
+          bodyText="Get 1-on-1 mentorship and career guidance through our summer program."
+          linkTo="/program"
+          isInternal={true}
+        />
+        <AboutCard
+          image={
+            <StaticImage
+              src="../images/ourTeam.png"
+              alt="our-program"
+              className="border border-black"
+            />
+          }
+          titleText="Our Leadership Team"
+          bodyText="Find out how we got started and what we're all about."
+          linkTo="/about"
+          isInternal={true}
+        />
+        <AboutCard
+          image={
+            <StaticImage
+              src="../images/ourMentors.png"
+              alt="our-program"
+              className="border border-black"
+            />
+          }
+          titleText="Our Mentors"
+          bodyText="Meet an amazing community of alumni and schedule mentorship sessions."
+          linkTo="https://mentorcenter.us/"
+          isInternal={false}
+        />
       </div>
     </div>
   );
