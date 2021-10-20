713c89ebc66a12cb5897b9ee4f12e0a28a2e4868
virenabhyankar <forvirenra@gmail.com>
2021-10-19 17:13:23 -0700

    Finish grid

--- src/components/UpcomingSummer.js
+++ src/components/UpcomingSummer.js
@@ -18,8 +18,12 @@ const UpcomingSummer = () => {
         <Subtitle>3 group sessions.</Subtitle>
         <Subtitle>6 weeks.</Subtitle>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2">
-        <div>
+      <div
+        className="pt-10 grid grid-cols-1 md:grid-cols-2 
+        md:gap-x-8 md:gap-y-12 lg:gap-y-14
+      w-72 md:w-auto md:max-w-2xl lg:max-w-4xl"
+      >
+        <div className="pb-6 justify-center flex flex-col">
           <h3 className="font-header text-mh3 lg:text-h3">Mentor Meetings</h3>
           <p className="font-body text-body text-left hidden md:block">
             Receive guidance about high school, college, and beyond from a
@@ -32,12 +36,12 @@ const UpcomingSummer = () => {
           alt="mentor-mentee"
           className="md:order-first"
         />
-        <p className="font-body text-body text-left md:hidden">
+        <p className="font-body text-body pt-5 text-left md:hidden">
           Receive guidance about high school, college, and beyond from a
           personal mentor in 1-on-1 sessions. Build a relationship that will
           last you a lifetime!
         </p>
-        <div>
+        <div className="pt-10 pb-6 justify-center flex flex-col">
           <h3 className="font-header text-mh3 lg:text-h3">Group Sessions</h3>
           <p className="font-body text-body text-left hidden md:block">
             Learn about the career pathway you’re interested in through
@@ -46,7 +50,7 @@ const UpcomingSummer = () => {
           </p>
         </div>
         <StaticImage src="../images/groupSessions.png" alt="mentor-mentee" />
-        <p className="font-body text-body text-left md:hidden">
+        <p className="font-body text-body text-left pt-5 md:hidden">
           Learn about the career pathway you’re interested in through
           specialized group sessions. Gain practical skills through workshops
           and hear from a variety of mentors about their experiences.
