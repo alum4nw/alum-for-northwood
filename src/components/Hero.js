0c9e16e91b206869b9c01983d6e2fe6aadea799d
virenabhyankar <forvirenra@gmail.com>
2022-01-15 19:12:24 -0800

    Fix spacing on home and 404

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,11 +10,11 @@ const Hero = () => {
         <div className="flex flex-col items-center">
           <Clouds className="lg:w-full" />
         </div>
-        <div className="absolute inset-0 text-center flex flex-col items-center justify-center">
-          <h1 className="font-header font-bold text-sh1 px-3 md:text-th1 md:font-bold lg:text-h1 pb-6 pt-36">
+        <div className="absolute inset-0 text-center flex flex-col items-center justify-center pt-32 md:pt-36">
+          <h1 className="font-header font-bold text-sh1 px-3 md:text-th1 md:font-bold lg:text-h1 pb-6 leading-tight md:leading-none">
             ALUM for Northwood
           </h1>
-          <h3 className="font-body text-mh4 lg:text-h4 pb-7 md:pb-14 px-11">
+          <h3 className="font-body text-mh4 lg:text-h4 pb-7 md:pb-14 px-11 pt-8 md:pt-0">
             Building a community through alumni mentorship.
           </h3>
           <a
